41fb6ab117d08781f207ecfdf91ebec05f8e443e
LewisTehMinerz <lewisminecraft@hotmail.co.uk>
2016-06-09 14:57:32 +0100

    Silence output from commands

--- misc/uninstall.bat
+++ misc/uninstall.bat
@@ -1,11 +1,12 @@
 @echo off
 echo We are now going to uninstall BetterDiscord, press any key to confirm.
-pause > nul
-del /f /q "%appdata%/BetterDiscord"
-del /f /q "%localappdata%/Discord/app-0.0.291/resources/node_modules/BetterDiscord"
-del /f /q "%localappdata%/Discord/app-0.0.291/resources/app"
+pause >nul
+del /f /q "%appdata%/BetterDiscord" 1>nul 2>nul
+del /f /q "%localappdata%/Discord/app-0.0.291/resources/node_modules/BetterDiscord" 1>nul 2>nul
+del /f /q "%localappdata%/Discord/app-0.0.291/resources/app" 1>nul 2>nul
 echo Done.
-taskkill /IM Discord.exe
+taskkill /f /im Discord.exe 1>nul 2>nul
 echo Restarting your Discord...
 start %localappdata%/Discord/app-0.0.291/Discord.exe
-echo Quitting...
+echo Cleaning up...
+start /b "" cmd /c del "%~f0"
