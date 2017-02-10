935d33a69126cdc6851ca1c08109494060e3f58b
Jiiks <jiiks@windowslive.com>
2017-02-10 17:49:31 +0200

    Some validation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 const { Storage, PluginStorage } = require('./storage');
 const { Logger } = require('./utils');
+const _logger = new Logger(true);
 const Config = require('./config');
 
 class Core {
 
     constructor(options) {
-        Logger.log(`v${Config.version} starting up`);
+        if(!this.validateOptions(options)) return;
+        _logger.log(`v${Config.version} starting up`);
+    }
+
+    validateOptions(options) 
+        if(options.mainWindow === undefined) {
+            _logger.log('MISSING PARAM: mainWindow', 'ERR');
+            return false;
+        }
+        return true;
     }
 
 }
