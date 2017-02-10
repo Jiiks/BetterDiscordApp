c8558b0894918ad1f5af8e559c4a3c06aa66228b
Jiiks <jiiks@windowslive.com>
2017-02-10 17:46:34 +0200

    Object logger

--- src/utils.js
+++ src/utils.js
@@ -5,13 +5,30 @@ class Utils {
 }
 
 class Logger {
+
+    constructor(debug) {
+        this.debug = debug || false;
+    }
+
     log(message, level) {
         level = level || 'INFO';
+        if(level === 'DBG') {
+            if(this.debug) this.actualLog(message, level);
+            return;
+        }
+        this.actualLog(message, level);
+    }
+
+    actualLog(message, level) {
+        if(typeof message === "object") {
+            console.log(`[BetterDiscord|${level}] - ${JSON.stringify(message)}`);
+            return;
+        }
         console.log(`[BetterDiscord|${level}] - ${message}`);
     }
 }
 
 module.exports = {
     Utils: new Utils(),
-    Logger: new Logger()
+    Logger: Logger
 };
