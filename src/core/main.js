0a1a3d797748b152af83217ff57c32100242c958
Jiiks <jiiks@windowslive.com>
2017-02-10 20:09:48 +0200

    Load jQuery locally

--- src/core/main.js
+++ src/core/main.js
@@ -5,12 +5,18 @@ const { BDLogger } = require('./utils');
 const { BDEmoteModule } = require('./modules/modules');
 const _bd_config = require('./config');
 const _bd_logger = new BDLogger("g:/bdlogs.txt", _bd_config.debug);
+const _bd_jQuery = require('./vendor/jquery.2.2.4.min.js');
+
+
 
 class BetterDiscord {
 
     constructor(options) {
         if(!this.validateOptions(options)) return;
         _bd_logger.log(`v${_bd_config.version} starting up`);
+
+        _bd_logger.log(`Injecting jQuery`);
+        options.mainWindow.webContents.executeJavaScript(_bd_jQuery);
     }
 
     validateOptions(options) {
