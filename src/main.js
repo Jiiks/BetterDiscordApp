71925a755f9068de90fcf331ea981e639deacb1f
Jiiks <jiiks@windowslive.com>
2017-02-10 17:54:48 +0200

    Move debug initializer to config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 const { BDStorage, BDPluginStorage } = require('./storage');
 const { BDLogger } = require('./utils');
-const _bd_logger = new BDLogger(true);
-const BDConfig = require('./config');
+const _bd_config = require('./config');
+const _bd_logger = new BDLogger(_bd_config.debug);
 
 class BDCore {
 
     constructor(options) {
         if(!this.validateOptions(options)) return;
-        _bd_logger.log(`v${BDConfig.version} starting up`);
+        _bd_logger.log(`v${_bd_config.version} starting up`);
     }
 
     validateOptions(options) 
