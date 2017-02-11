7aa1e43aba6794ad2a6e47828abb36df9175c510
Jiiks <jiiks@windowslive.com>
2017-02-11 21:23:44 +0200

    remove changehandler from settingspanel component

--- src/client/components/settingsPanel.js
+++ src/client/components/settingsPanel.js
@@ -44,11 +44,6 @@ class CSettingsPanel extends Component {
         });
     }
 
-
-    handleChange(key, checked) {
-        this.props.handleChange(key, checked);
-    }
-
 }
 
 export default CSettingsPanel;
