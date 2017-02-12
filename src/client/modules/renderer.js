24a700323147ac362ca4782782a10d5abd1e676a
Jiiks <jiiks@windowslive.com>
2017-02-12 19:41:40 +0200

    Comments

--- src/client/modules/renderer.js
+++ src/client/modules/renderer.js
@@ -6,22 +6,24 @@ const $ = require('../vendor/jquery');
 
 class Renderer  {
     
-    constructor() {
+    constructor() { }
 
-    }
-
+    //Get react instance
     static get react() {
         return React;
     }
 
+    //Get react-dom instance
     static get reactDom() {
         return ReactDOM;
     }
 
+    //Render a raw component
     static rawRender(...args) {
         ReactDOM.render(...args);
     }
 
+    //Main render method, returns the root element, component and removal function
     static render(root, component) {
         let re = ReactDOM.render(component, root[0]);
         return {
@@ -31,37 +33,43 @@ class Renderer  {
         }
     }
 
+    //Insert root element before supplied selector
     static insertBefore(selector, root, component) {
         root.insertBefore($(selector));
         return this.render(root, component);
     }
 
+    //Insert root element after supplied selector
     static insertAfter(selector, root, component) {
         root.insertAfter($(selector));
         return this.render(root, component);
     }
 
-
+    //Append root element to supplied selector
     static append(selector, root, component) {
         root.append($(selector));
         return this.render(root, component);
     }
 
+    //Append root element to supplied selector
     static appendTo(selector, root, component) {
         root.appendTo($(selector));
         return this.render(root, component);
     }
 
+    //Prepend root element to supplied selector
     static prepend(selector, root, component) {
         $(selector).prepend(root);
         return this.render(root, component);
     }
 
+    //Prepend root element to supplied selector
     static prependTo(selector, root, component) {
         $(selector).prepend(root);
         return this.render(root, component);
     }
 
+    //Replace supplied selector with root
     static replaceWith(selector, root, component) {
         $(selector).replaceWith(root);
         return this.render(root, component);
