fc89162a948b65377bcc2cc3af39279414157405
Jiiks <jiiks@windowslive.com>
2017-02-12 19:50:28 +0200

    Headers

--- src/client/vendor/react.js
+++ src/client/vendor/react.js
@@ -1,4 +1,15 @@
-"use strict";
+/**
+ * BetterDiscord React vendor
+ * Exports React to be used in other classes
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+'use strict';
 
 const ReactInstance = require('react');
 
--- src/client/vendor/reactdom.js
+++ src/client/vendor/reactdom.js
@@ -1,5 +1,16 @@
-"use strict";
+/**
+ * BetterDiscord react-dom vendor
+ * Exports react-dom to be used in other classes
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
 
-const ReactDOMInstance = require("ReactDOM");
+'use strict';
+
+const ReactDOMInstance = require('ReactDOM');
 
 module.exports = ReactDOMInstance;
