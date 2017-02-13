f5b2fb448c413b76f7555ffc8a7d57bdc86a9342
Jiiks <jiiks@windowslive.com>
2017-02-13 23:32:51 +0200

    Component exports

---
+++ src/client/components/index.js
@@ -0,0 +1,29 @@
+/**
+ * BetterDiscord Components Export
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+'use strict';
+
+import CButton from './button';
+import CCheckBox from './checkbox';
+import CCheckboxGroup from './checkboxGroup';
+import CLink from './link';
+import CPluginList from './pluginlist';
+import CProTip from './protip';
+import CSettingsPanel from './settingsPanel';
+
+module.exports = {
+    CButton: CButton,
+    CCheckBox: CCheckBox,
+    CCheckboxGroup: CCheckboxGroup,
+    CLink: CLink,
+    CPluginList: CPluginList,
+    CProTip: CProTip,
+    CSettingsPanel: CSettingsPanel
+}
