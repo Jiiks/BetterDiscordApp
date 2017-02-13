38d876c3d02ff2f79d6514abe4f51f3a64859bfb
Jiiks <jiiks@windowslive.com>
2017-02-13 23:34:33 +0200

    Static conversion

--- src/client/modules/settings.js
+++ src/client/modules/settings.js
@@ -10,32 +10,48 @@
 
 'use strict';
 
-const ISingleton = require('../interfaces/isingleton');
-
 const fs = require('fs');
 
+class SettingsModule {
 
-class SettingsModule extends ISingleton {
+    constructor() {}
 
-    constructor(filePath) {
-        super("settingsmodule");
-        this.filePath = "g:/bdsettings.json"; //Static path for testing
+    static init(options) {
+        if(this.filePath !== undefined) {
+            console.log("Attempt to reinitialize SettingsModule has been blocked");
+            return;
+        }
+        this.filePath = options.filePath;
+        this.load();
     }
 
-    load() {
+    static load() {
         this.settings = JSON.parse(fs.readFileSync(this.filePath));
     }
 
-    save() {
+    static save() {
         fs.writeFileSync(this.filePath, JSON.stringify(this.settings));
     }
 
-    getCoreSetting(key)  { return this.getSetting("core",   key); }
-    getUiSetting(key)    { return this.getSetting("ui",     key); }
-    getEmoteSetting(key) { return this.getSetting("emotes", key); }
-    getSetting(sub, key) {
+    static get getCoreSettings() { return this.getSettings("core"); }
+    static get getUiSettings() { return this.getSettings("ui"); }
+    static get getEmoteSettings() { return this.getSettings("emotes"); }
+    static getSettings(key) { return this.settings[key]; }
+
+    static getCoreSetting(key)  { return this.getSetting("core",   key); }
+    static getUiSetting(key)    { return this.getSetting("ui",     key); }
+    static getEoteSetting(key) { return this.getSetting("emotes", key); }
+    static getSetting(sub, key) {
         return this.settings[sub].filter(value => value.key === key)[0];
+    }
+
+    static setCoreSetting(key, enabled) { this.setSetting("core", key, enabled); }
+    static setUiSetting(key, enabled) { this.setSetting("ui", key, enabled); }
+    static setEmoteSetting(key, enabled) { this.setSetting("emotes", key, enabled); }
+    static setSetting(sub, key, enabled) {
+        this.getSetting(sub, key).enabled = enabled;
+        this.save();
     }
 }
 
-module.exports = new SettingsModule(); 
+module.exports = SettingsModule; 
