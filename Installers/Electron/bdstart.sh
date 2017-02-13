7ea0c82b3edb11044a3dca0c74618c1ea372e651
Serkan Devel <patently.paul@gmail.com>
2017-02-13 20:37:53 +0100

    remove emotefilter

--- Installers/Electron/bdstart.sh
+++ Installers/Electron/bdstart.sh
@@ -1,1 +1,1 @@
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device /dev/snd dixord
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/snd dixord
