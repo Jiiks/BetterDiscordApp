a5668eef87ba889f27ba31b63fff1adabc3de1d6
Jiiks <jiiks@windowslive.com>
2017-02-13 23:34:47 +0200

    moment.js export

---
+++ src/client/vendor/moment.js
@@ -0,0 +1,16 @@
+/**
+ * BetterDiscord jQuery vendor
+ * Exports jQuery to be used in other classes
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+'use strict';
+
+const momentInstance = window._bd.moment;
+
+module.exports = momentInstance;
