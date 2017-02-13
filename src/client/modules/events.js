e60b022f12b38fdeca6053ea3cd566c77f412350
Jiiks <jiiks@windowslive.com>
2017-02-13 23:33:14 +0200

    Headers

--- src/client/modules/events.js
+++ src/client/modules/events.js
@@ -1,3 +1,15 @@
+/**
+ * BetterDiscord Events Module
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+'use strict';
+
 const ISingleton = require('../interfaces/isingleton');
 
 const { EventEmitter } = require('events');
