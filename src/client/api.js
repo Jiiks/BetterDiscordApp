5479c4849b87be91c48b2f3d9dbb1e8c3f8154ac
Jiiks <jiiks@windowslive.com>
2017-02-13 23:32:42 +0200

    Api base

---
+++ src/client/api.js
@@ -0,0 +1,26 @@
+/**
+ * BetterDiscord Api
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+const { Message, Channel, User } = require('./api/structs');
+
+
+class Api {
+
+    constructor() {
+        this.structs = {
+            Message: Message,
+            Channel: Channel,
+            User: User
+        }
+    }
+
+}
+
+module.exports = new Api();
