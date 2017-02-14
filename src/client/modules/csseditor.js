ff9634816dcfd5f967e039c0adde14ddb6a3a579
Jiiks <jiiks@windowslive.com>
2017-02-14 09:16:14 +0200

    css editor module

---
+++ src/client/modules/csseditor.js
@@ -0,0 +1,67 @@
+/**
+ * BetterDiscord CSS Editor Module
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+*/
+
+'use strict';
+
+class CssEditor {
+
+    constructor() {
+        let self = this;
+
+        self.editor = {
+            open: false,
+            options: {
+                'frame': false, 
+                'minWidth': 700, 
+                'minHeight': 400
+            }
+        };
+    }
+
+    open() {
+        let self = this;
+        if(self.editor.open && self.editor.window) {
+            self.editor.window.focus();
+            self.editor.window.flashFrame(true);
+            return;
+        }
+
+        self.editor.window = new remote.BrowserWindow(self.editor.options);
+        self.loadURL("file://g:/bd/data/csseditor.html"); //Static path for testing
+        self.editor.window.on('close', self.onClose);
+        self.editor.window.on('resize', self.onResize);
+        self.editor.window.on('move', self.onMove);
+    }
+
+    loadURL(url) {
+        if(!self.editor.window) return;
+        self.editor.window.loadURL(url);
+    }
+
+    onClose() {
+        self.editor.open = false;
+        self.editor.window = null;
+    }
+
+    onResize() {
+        let {width, height} = self.editor.window.getBounds();
+        self.editor.options.width = width;
+        self.editor.options.height = height;
+    }
+
+    onMove() {
+        let {x, y} = self.editor.window.getBounds();
+        self.editor.options.x = x;
+        self.editor.options.y = y;
+    }
+
+}
+
+module.exports = CssEditor;
