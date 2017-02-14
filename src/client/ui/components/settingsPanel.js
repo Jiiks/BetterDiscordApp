86165ecc86fe495dde9ef7d8a77e69bcd772a497
Jiiks <jiiks@windowslive.com>
2017-02-14 08:53:32 +0200

    add buttons to panel

--- src/client/ui/components/settingsPanel.js
+++ src/client/ui/components/settingsPanel.js
@@ -29,14 +29,22 @@ class CSettingsPanel extends Component {
     }
 
     render() {
+
         return (
             <div className="scroller-wrap">
                 <div className="scroller settings-wrapper settings-panel user-settings-text-chat">
                     <div className="tab-bar TOP">
                         {this.props.tabs.map(value => {
                         let cn = this.state.activeTab === value.key ? "tab-bar-item selected" : "tab-bar-item";
-                        return <div key={value.key} onClick={() => this.switchTab(value.key)} className={cn}>{value.text}</div>
+                        return <div key={value.key} onClick={() => {value.onClick ? value.onClick(value.key) : this.switchTab(value.key)}} className={cn}>{value.text}</div>
                         })}
+                        {(this.props.topbuttons !== undefined) &&
+                        <div style={{position: "absolute", top: "24px", right: "20px"}}>
+                        {this.props.topbuttons.map(value => {
+                            return <div key={value.key} style={{height: "15px", fontSize: "10px", padding: "3px 6px"}} onClick={value.onClick} className="btn btn-primary">{value.text}</div>
+                        })}
+                        </div>
+                        }
                     </div>
                     <div className="control-groups">
                         {this.props.content[this.state.activeTab]}
