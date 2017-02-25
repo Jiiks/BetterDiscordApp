3ffa45c913768af30c75add16c8ccfef768cfab4
Jiiks <jiiks@windowslive.com>
2017-02-25 19:55:23 +0200

    emote module base

---
+++ src/client/modules/emotes.js
@@ -0,0 +1,31 @@
+/**
+ * BetterDiscord Emote Module
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+*/
+
+'use strict';
+
+const Events = require('./events');
+
+class Emotes {
+
+    constructor() {
+        let self = this;
+
+        Events.on('new-message', message => {
+            self.inspectMessage(messaage);
+        });
+
+    }
+
+    inspectMessage(message) {
+
+    }
+}
+
+module.exports = Emotes;
