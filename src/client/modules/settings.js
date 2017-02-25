59be69536bad03e6540293bd7087c1926a3ac147
Jiiks <jiiks@windowslive.com>
2017-02-25 18:37:39 +0200

    Correct path and static defaults for preview

--- src/client/modules/settings.js
+++ src/client/modules/settings.js
@@ -12,7 +12,116 @@
 
 const IPC = require('./ipc');
 const Utils = require('./utils');
-const defaultSettings = require('../data/user.settings.default');
+const defaultSettings = {
+    "core": [
+        {
+            "key": "voice-disconnect",
+            "text": "Voice Disconnect",
+            "helptext": "Disconnect from voice server when Discord closes",
+            "enabled": false
+        },
+        {
+            "key": "developer-mode",
+            "text": "Developer Mode",
+            "helptext": "BetterDiscord developer mode",
+            "enabled": false
+        }
+    ],
+    "ui": [
+        {
+            "key": "public-servers",
+            "text": "Public Servers",
+            "helptext": "Display public servers button",
+            "enabled": false
+        },
+        {
+            "key": "minimal-mode",
+            "text": "Minimal Mode",
+            "helptext": "Hide elements and reduce size of certain elements",
+            "enabled": false
+        },
+        {
+            "key": "voice-mode",
+            "text": "Voice Mode",
+            "helptext": "Only show voice chat",
+            "enabled": false
+        },
+        {
+            "key": "hide-channels",
+            "text": "Hide Channels",
+            "helptext": "Hide server channels in minimal mode",
+            "enabled": false
+        },
+        {
+            "key": "dark-mode",
+            "text": "Dark Mode(wip)",
+            "helptext": "Make certain elements dark by default",
+            "enabled": false
+        },
+        {
+            "key": "timestamp",
+            "text": "24 Hour Timestamps",
+            "helptext": "Replace 12 hour timestamps with proper ones",
+            "enabled": false
+        },
+        {
+            "key": "coloured-text",
+            "text": "Coloured Text",
+            "helptext": "Make text colour the same as role colour",
+            "enabled": false
+        }
+    ],
+    "emotes": [
+        {
+            "key": "twitch-emotes",
+            "text": "Twitch Emotes",
+            "helptext": "Show Twitch emotes",
+            "enabled": false
+        },
+        {
+            "key": "ffz-emotes",
+            "text": "FrankerFaceZ Emotes",
+            "helptext": "Show FrankerFaceZ emotes",
+            "enabled": false
+        },
+        {
+            "key": "bttv-emotes",
+            "text": "BetterTTV Emotes",
+            "helptext": "Show BetterTTV emotes",
+            "enabled": false
+        },
+        {
+            "key": "emote-menu",
+            "text": "Emote Menu",
+            "helptext": "Show Twitch/Favourite emotes in emote menu",
+            "enabled": false
+        },
+        {
+            "key": "emoji-menu",
+            "text": "Emoji Menu",
+            "helptext": "Show Discord emoji menu",
+            "enabled": false
+        },
+        {
+            "key": "emote-autocap",
+            "text": "Emote Auto Capitalization",
+            "helptext": "Automatically capitalize emotes as you type",
+            "enabled": false
+        },
+        {
+            "key": "emote-tooltips",
+            "text": "Emote Tooltips",
+            "helptext": "Show emote tooltips when you hover over them",
+            "enabled": false
+        },
+        {
+            "key": "emote-modifiers",
+            "text": "Emote Modifiers",
+            "helptext": "Enable emote modifiers",
+            "enabled": false
+        }
+    ]
+}
 
 class SettingsModule {
 
@@ -27,29 +136,28 @@ class SettingsModule {
         }
 
         let getSettings = IPC.sendSync({ 'command': 'getconfig' });
+        self.settings = getSettings.data;
 
-        let { dataPath } = getSettings.data;
-        console.log(dataPath);
-        self.filePath = `${dataPath}/user.settings.json`;
         self.load();
     }
 
     static load() {
         let self = this;
-        let read = Utils.readFileSync(self.filePath);
+        let read = Utils.readFileSync(`${self.settings.dataPath}/user.settings.json`);
 
         if(!read) {
-            self.settings = defaultSettings;
+            self.userSettings = defaultSettings;
             return;
         }
 
         let settings = Utils.tryParse(read);
         
-        self.settings = settings || defaultSettings;
+        self.userSettings = settings || defaultSettings;
     }
 
     static save() {
-        if(!Utils.writeFileSync(this.filePath, JSON.stringify(this.settings))) {
+        let self = this;
+        if (!Utils.writeFileSync(`${self.settings.dataPath}/user.settings.json`, JSON.stringify(self.userSettings))) {
             //TODO replace with Logger
             console.log("Failed to write settings file!");
         }
@@ -58,13 +166,13 @@ class SettingsModule {
     static get getCoreSettings() { return this.getSettings("core"); }
     static get getUiSettings() { return this.getSettings("ui"); }
     static get getEmoteSettings() { return this.getSettings("emotes"); }
-    static getSettings(key) { return this.settings[key]; }
+    static getSettings(key) { return this.userSettings[key]; }
 
     static getCoreSetting(key)  { return this.getSetting("core",   key); }
     static getUiSetting(key)    { return this.getSetting("ui",     key); }
     static getEoteSetting(key) { return this.getSetting("emotes", key); }
     static getSetting(sub, key) {
-        return this.settings[sub].filter(value => value.key === key)[0];
+        return this.userSettings[sub].filter(value => value.key === key)[0];
     }
 
     static setCoreSetting(key, enabled) { this.setSetting("core", key, enabled); }
