f0d67695bf59f2c8256a66830510d97d21e7fb19
Jiiks <jiiks@windowslive.com>
2017-02-25 18:35:17 +0200

    Proper paths

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,39 @@
+/**
+ * BetterDiscord Client Core
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
 'use strict';
+
+const { Observer, Settings, IPC, Events, PluginManager, Utils, Logger } = require('./modules');
+const Api = require('./api');
+const { SettingsPanel, ContextMenu } = require('./ui');
+const { $ } = require('./vendor');
 
 class BDCore {
 
     constructor() {
-        this.options = window._bd.options;
-        this.ws = window._bd.fastHook.ws;
-        this.ipc = window._bd.ipc;
-        this.$ = this.jQuery = window._bd.jQuery;
-        if(this.options.debug) {
-            window._bdDebug = window._bd;
-            window.BD = window._bd;
-        }
-        window._bd = null;
+
+        Settings.init();
+        
+        let css = Utils.readFileSync(`${Settings.settings.dataPath}/betterdiscord.css`);
+
+        $("head").append(`<style>${css}</style>`);
+
+        PluginManager.init();
+
+        window.onbeforeunload = (e) => {
+            IPC.send({ "command": "reset" });
+        };
+
+        Observer.observe({ 'childList': true, 'subtree': true });
+
+        let settingsPanel = new SettingsPanel();
     }
 
 }
