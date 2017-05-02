36119cf73a3cfe53535e5df1cbd235d1788785d8
Jiiks <jiiks@windowslive.com>
2017-05-02 07:36:24 +0300

    Get current guild and channel

--- src/client/modules/browserevents.js
+++ src/client/modules/browserevents.js
@@ -10,35 +10,73 @@
 
 'use strict';
 
+const { $ } = require('../vendor');
+const { Guild, Channel } = require('../api/structs');
 const Events = require('./events');
+const DeepReflection = require('./reflection_deep');
+const Api = require('./api');
+const Logger = require('./logger');
 
 class BrowserEvents {
 
     constructor() {
         let self = this;
-        self.prev = {
-            'server': undefined,
-            'channel': undefined
-        }
+
+		self.page = {
+			'previous': {
+				'guild': Api.currentGuild,
+				'channel': Api.currentChannel
+			},
+			'current': this.prev
+		};
+
+
         Events.on('browser-event', e => {
             let { type, url } = e;
             switch (type) {
                 case 'did-navigate-in-page':
-                    let parsed = self.parseUrl(url);
-                    if (parsed.server !== self.prev.server) {
-                        Events.emit('server-switch');
-                        self.prev = parsed;
-                        return;
-                    }
-                    if (parsed.channel !== self.prev.channel) {
-                        Events.emit('channel-switch');
-                        self.prev = parsed;
-                        return;
-                    }
-                    break;
+                    self.didNavigateInPage(url);
+					break;
             }
         });
     }
+
+	didNavigateInPage(url) {
+		let self = this;
+
+		self.page.current = {
+			'guild': Api.currentGuild,
+			'channel': Api.currentChannel
+		};
+
+		if(!self.page.previous.guild) {
+			Logger.debug("BrowserEvents", "Server Switch");
+			console.log(self.page);
+			self.page.previous = self.page.current;
+			return;
+		}
+
+		if(self.page.previous.guild.id !== self.page.current.guild.id) {
+			Logger.debug("BrowserEvents", "Server Switch");
+			console.log(self.page);
+			self.page.previous = self.page.current;
+			return;
+		}
+
+		if(!self.page.previous.channel) {
+			Logger.debug("BrowserEvents", "Channel Switch");
+			console.log(self.page);
+			self.page.previous = self.page.current;
+			return;
+		}
+
+		if(self.page.previous.channel.id !== self.page.current.channel.id) {
+			Logger.debug("BrowserEvents", "Channel Switch");
+			console.log(self.page);
+			self.page.previous = self.page.current;
+			return;
+		}
+	}
     
     parseUrl(url) {
         let splice = url.split('/').splice(4);
