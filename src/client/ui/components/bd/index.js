5e2abd1f4073323ce17a1e2937ed952d8a8f8f2a
Jiiks <jiiks@windowslive.com>
2017-05-02 07:36:38 +0300

    Exports

---
+++ src/client/ui/components/bd/index.js
@@ -0,0 +1,12 @@
+/**
+ * BetterDiscord Core Components Export
+ * Copyright (c) 2015-present Jiiks - https://jiiks.net
+ * All rights reserved.
+ * https://github.com/Jiiks/BetterDiscordApp - https://betterdiscord.net
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
+*/
+
+export { default as CSP_Sidebar } from './settingspanel_sidebar';
+export { default as CSP_Content } from './settingspanel_content';
