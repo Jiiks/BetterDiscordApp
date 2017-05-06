ff61c4e054862cd9220c5f8586c5a904373b8848
Jiiks <jiiks@windowslive.com>
2017-05-06 04:25:50 +0300

    Parent state rendering

--- src/client/ui/components/pluginPanel.js
+++ src/client/ui/components/pluginPanel.js
@@ -6,7 +6,7 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree. 
- */
+*/
 
 'use strict';
 
@@ -18,12 +18,24 @@ class CPluginPanel extends Component {
 
     constructor(props) {
         super(props);
+        this.bindings();
+        this.setInitialState();
+    }
+
+    bindings() {
+        this.settingsHandler = this.settingsHandler.bind(this);
+    }
+
+    setInitialState() {
+        this.state = {
+            'settingsOpen': null
+        };
     }
 
     render() {
         let self = this;
         let { plugins } = self.props;
-        window.pl = plugins;
+
         return (
             <div className="content-column default">
                 <h2 className="ui-form-title h2 margin-reset margin-bottom-20">Plugins</h2>
@@ -37,23 +49,18 @@ class CPluginPanel extends Component {
                         </div>
                     </div>
                     {plugins.map(plugin => {
-                        return <CPluginCard key={plugin.name} plugin={plugin} />
-                    })}
-                    {plugins.map(plugin => {
-                        return <CPluginCard key={plugin.name} plugin={plugin} />
-                    })}
-                    {plugins.map(plugin => {
-                        return <CPluginCard key={plugin.name} plugin={plugin} />
-                    })}
-                    {plugins.map(plugin => {
-                        return <CPluginCard key={plugin.name} plugin={plugin} />
-                    })}
-                    {plugins.map(plugin => {
-                        return <CPluginCard key={plugin.name} plugin={plugin} />
+                        return <CPluginCard settingsHandler={self.settingsHandler} settings={plugin.name === self.state.settingsOpen} key={plugin.name} plugin={plugin} />
                     })}
                 </div>
             </div>
         );
+    }
+
+    settingsHandler(id) {
+        if (id === this.state.settingsOpen) id = null;
+        this.setState({
+            'settingsOpen': id
+        });
     }
 }
 
