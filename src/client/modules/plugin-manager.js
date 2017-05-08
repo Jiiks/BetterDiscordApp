b5fec5d58088a49559d58c03bcaa5f523b16c8b3
Jiiks <jiiks@windowslive.com>
2017-05-08 10:13:56 +0300

    Plugin manager refactoring

--- src/client/modules/plugin-manager.js
+++ src/client/modules/plugin-manager.js
@@ -8,30 +8,29 @@
  * LICENSE file in the root directory of this source tree.
 */
 
-const { Plugin, PluginApi, PluginStorage, PluginEvents } = require('../plugins');
-const { jQuery, React } = require('../vendor');
-const IPC = require('./ipc');
 
+const { Plugin, PluginApi, PluginStorage, PluginEvents } = require('../plugins');
+const { jQuery, React, moment } = require('../vendor');
+const IPC = require('./ipc');
 const Utils = require('./utils');
 const Logger = require('./logger');
-//const Events = require('./events');
 const Settings = require('./settings');
 
 const Plugins = [];
 
 const Vendor = {
     'jQuery': jQuery,
+    '$': jQuery,
     'React': React,
-    '$': jQuery
+    'moment': moment
 };
 
-//Heuristic keyword ban
 const blockedKeywords = {
-    'token':        'ACCESS USER TOKEN',
+    'token': 'ACCESS USER TOKEN',
     'localstorage': 'ACCESS LOCALSTORAGE',
-    'require':      'REQUIRE MODULE',
-    'iframe':       'CREATE IFRAMES',
-    'eval':         'EVALUATE CODE'
+    'require': 'REQUIRE MODULE',
+    'iframe': 'CREATE IFRAMES',
+    'eval': 'EVALUATE CODE'
 };
 
 const authorizedPlugins = [];
@@ -39,39 +38,44 @@ const authorizedPlugins = [];
 class PluginManager {
 
     constructor() {
-        let self = this;
-        self.pluginPath = `${Settings.settings.basePath}/plugins`;
-        self.loadPlugins(() => { });
+        this.loadPlugins(() => { });
+    }
+
+    get pluginPath() {
+        return `${Settings.settings.basePath}/plugins`;
+    }
+
+    get plugins() {
+        return Plugins;
     }
 
     loadPlugins(cb) {
-        let self = this;
-        Utils.readDir(self.pluginPath, files => {
+        Utils.readDir(this.pluginPath, files => {
             if (!files) {
-                cb(self.plugins);
+                cb(this.plugins);
                 return;
             }
+
             files.forEach(file => {
-                self.loadPluginv2(file, false, true);
-                //if (!file.endsWith('.plugin.js')) return;
-                //self.loadPlugin(file.replace('.plugin.js', ''));
+                this.loadPlugin(file, false, true);
             });
 
-            cb(self.plugins);
+            cb(this.plugins);
         });
     }
 
-    loadPluginv2(name, reload, all, cb) {
-        let self = this;
 
-        let basePath = `${self.pluginPath}/${name}`;
 
-        if (self.getPlugin(name) && !reload) {
+    loadPlugin(name, reload, all, cb) {
+
+        const basePath = `${this.pluginPath}/${name}`;
+
+        if (this.getPlugin(name) && !reload) {
             if (!all) Logger.log('PluginManager', `Attempted to load already loaded plugin: ${name}`, 'warn');
             return;
         }
 
-        let config = Utils.tryParse(Utils.readFileSync(`${basePath}/config.json`));
+        const config = Utils.tryParse(Utils.readFileSync(`${basePath}/config.json`));
 
         if (!config) {
             Logger.log('PluginManager', `Failed to load config for: ${name}`, 'err');
@@ -79,24 +83,28 @@ class PluginManager {
         }
 
         Utils.readDir(basePath, files => {
-            let pluginFile = files.find(file => file.endsWith('.js'));
-            if (!self.validatePlugin(`${basePath}/${pluginFile}`)) return;
+            const pluginFile = files.find(file => file.endsWith('.js'));
+            const absPath = `${basePath}/${pluginFile}`;
+            if (!this.validatePlugin(absPath)) {
+                console.log("INVALID PLUGIN");
+                return;
+            }
 
-            if (reload) delete window.require.cache[window.require.resolve(`${basePath}/${pluginFile}`)];
+            if (reload) delete window.require.cache[window.require.resolve(absPath)];
 
-            let storage = new PluginStorage(basePath, config.defaultSettings);
+            const storage = new PluginStorage(basePath, config.defaultSettings);
 
-            let BD = {
+            const BD = {
                 'Api': new PluginApi(config.info),
                 'Storage': storage,
                 'Events': PluginEvents
-            }
+            };
 
             let plugin = null;
             let pluginInstance = null;
 
             try {
-                plugin = window.require(`${basePath}/${pluginFile}`)(Plugin, BD, Vendor);
+                plugin = window.require(absPath)(Plugin, BD, Vendor);
                 pluginInstance = new plugin(config.info);
             } catch (err) {
                 Logger.log('PluginManager', `Failed to load plugin: ${name} - ${err.message}`, 'err');
@@ -110,112 +118,33 @@ class PluginManager {
             };
 
             if (reload) {
-                let index = self.getPluginIndex(name);
-                self.plugins[index] = pluginInstance;
+                const index = this.getPluginIndex(name);
+                this.plugins[index] = pluginInstance;
             } else {
-                self.plugins.push(pluginInstance);
+                this.plugins.push(pluginInstance);
             }
 
             if (pluginInstance.internal.storage.getSetting('enabled')) pluginInstance.onStart();
-            if(cb) cb(pluginInstance);
-        });
-    }
-
-    getPluginIndex(name) {
-        return this.plugins.findIndex(plugin => { return (plugin.name === name || plugin.internal.path === name); });
-    }
-
-    getPlugin(name) {
-        return this.plugins.find(plugin => { return (plugin.name === name || plugin.internal.path === name); });
-    }
-
-    loadPlugin(name, reload) {
-        let self = this; 
-
-        if (self.plugins.hasOwnProperty(name) && !reload) return;
-
-        let path = `${self.pluginPath}/${name}.plugin.js`;
-
-        if (!self.validatePlugin(path)) return;
-
-        if (reload) delete window.require.cache[window.require.resolve(path)];
-
-        let plugin = null;
-        let pluginInstance = null;
-        try {
-            plugin = window.require(path)(Plugin);
-            pluginInstance = new plugin();
-        } catch (err) {
-            Logger.log('PluginLoader', `Failed to load plugin: ${name} - ${err.message}`, 'err');
-            console.log(err.stack);
-            return;
-        }
-
-        pluginInstance.id = name;
-
-        Plugins[name] = pluginInstance;
-
-        let storage = new PluginStorage({ 'basePath': self.pluginPath, 'name': name, 'defaults': pluginInstance.defaultConfig });
-        storage.load();
-
-        pluginInstance.onLoad({
-            'Api': new PluginApi(pluginInstance.props),
-            'Vendor': Vendor,
-            'Storage': storage,
-            'Events': PluginEvents
+            if (cb) cb(pluginInstance);
         });
 
-        pluginInstance.internal = {
-            'storage': storage
-        };
-
-        if (!storage.getSetting('enabled')) storage.setSetting('enabled', false);
-        if (storage.getSetting('enabled')) pluginInstance.onStart();
-
-        return pluginInstance;
-    }
-
-    validatePlugin(path) {
-        let pluginData = Utils.readFileSync(path);
-        if (!pluginData) {
-            Logger.log('PluginLoader', `Attempted to load a plugin that does not seem to exist: ${path}`, 'warn');
-            return false;
-        }
-        pluginData = pluginData.toLowerCase();
-
-        if (Object.keys(blockedKeywords).some(key => {
-            if (pluginData.indexOf(key) !== -1) {
-                Logger.log('PluginLoader', `BLOCKED LOADING OF PLUGIN ATTEMPTING TO ${blockedKeywords[key]}`, 'err');
-                return true;
-            }
-        })) return false;
-
-        //HASH VALIDATION IS DISABLED DURING DEVELOPMENT
-       /* let hash = IPC.sendSync({ 'command': 'md5', 'data': path }).data;
-        if (!authorizedPlugins.includes(hash)) {
-            Logger.log('PluginLoader', 'BLOCKED LOADING OF UNAUTHORIZED PLUGIN', 'err');
-            return false;
-        }*/
-
-        return true;
     }
 
     reloadPlugin(id, cb) {
-        let self = this;
-        let plugin = self.getPlugin(id);
-        if(!plugin) {
+        const plugin = this.getPlugin(id);
+        if (!plugin) {
             Logger.log('PluginManager', `Attempted to reload a plugin that is not loaded: ${id}`, 'warn');
-            return null;
+            if (cb) cb(null);
+            return;
         }
 
         if (plugin.internal.storage.getSetting('enabled')) plugin.onStop();
 
-        this.loadPluginv2(plugin.internal.path, true, false, cb);
+        this.loadPlugin(plugin.internal.path, true, false, cb);
     }
 
     startPlugin(id) {
-        let self = this;
-        let plugin = self.getPlugin(id);
+        const plugin = this.getPlugin(id);
         if (!plugin) {
             Logger.log('PluginManager', `Attempted to start a plugin that is not loaded: ${id}`, 'err');
             return;
@@ -227,8 +156,7 @@ class PluginManager {
     }
 
     stopPlugin(id) {
-        let self = this;
-        let plugin = self.getPlugin(id);
+        const plugin = this.getPlugin(id);
         if (!plugin) {
             Logger.log('PluginManager', `Attempted to stop a plugin that is not loaded: ${id}`, 'err');
             return;
@@ -239,8 +167,41 @@ class PluginManager {
         return true;
     }
 
-    get plugins() {
-        return Plugins;
+    getPluginIndex(name) {
+        return this.plugins.findIndex(plugin => { return (plugin.name === name || plugin.internal.path === name); });
+    }
+
+    getPlugin(name) {
+        return this.plugins.find(plugin => { return (plugin.name === name || plugin.internal.path === name); });
+    }
+
+    validatePlugin(path) {
+        let pluginData = Utils.readFileSync(path);
+
+        if (!pluginData) {
+            Logger.log('PluginLoader', `Attempted to load a plugin that does not seem to exist: ${path}`, 'warn');
+            return false;
+        }
+
+        pluginData = pluginData.toLowerCase();
+
+        if (Object.keys(blockedKeywords).some(key => {
+            if (pluginData.indexOf(key) !== -1) {
+                Logger.log('PluginLoader', `BLOCKED LOADING OF PLUGIN ATTEMPTING TO ${blockedKeywords[key]}`, 'err');
+                return false;
+            }
+        }));
+
+        return true;
+    }
+
+    validateHash(path) {
+        const hash = IPC.sendSync({ 'command': 'md5', 'data': path }).data;
+        if (!authorizedPlugins.includes(hash)) {
+            Logger.log('PluginLoader', 'BLOCKED LOADING OF UNAUTHORIZED PLUGIN', 'err');
+            return false;
+        }
+        return true;
     }
 
 }
