1d305fd45e98bc1bd4d4ef21374baec374cf2b5f
Jiiks <jiiks@windowslive.com>
2017-05-16 08:34:26 +0300

    Update logger styling

--- src/client/modules/logger.js
+++ src/client/modules/logger.js
@@ -10,8 +10,9 @@
 'use strict';
 
 class Logger {
-    static log(moduleName, message, level = 'info') {
-        console.log(`%c[%cBetter%cDiscord%c:${moduleName}%c]%c ${message} `, ...this.style(level));
+
+    static log(moduleName, message, level = 'log') {
+        console[level]('[%cBetter%cDiscord:%s] %s', 'color: #3E82E5', '', `${moduleName}${level === 'debug' ? '|DBG' : ''}`, message);
     }
 
     static logObject(moduleName, message, object, level) {
@@ -21,45 +22,16 @@ class Logger {
 
     static debug(moduleName, message, level, force) {
         if (!force) { if (!window.BetterDiscord || !window.BetterDiscord.debug) return; }
-		
-		console.log(`%c[%cBetter%cDiscord%c:${moduleName}%c|DBG]%c ${message} `, ...this.style(level || 'info'));
+        this.log(moduleName, message, 'debug', true);
     }
 
     static debugObject(moduleName, message, object, level, force) {
         if (!force) { if (!window.BetterDiscord || !window.BetterDiscord.debug) return; }
 
         if (message) this.debug(moduleName, message, level, force);
-        console.log(object);
+        console.debug(object);
     }
 
-    static style(level) {
-        return {
-            'info': [
-                'padding: 1px; color: #FFFFFF;',
-                'padding: 1px; color: #3E82E5;',
-                'padding: 1px; color: #FFFFFF;',
-                'padding: 1px; color: #FFFFFF;',
-                'padding: 1px; color: #FFFFFF;',
-                'padding: 1px; color: #FFFFFF;'
-            ],
-            'warn': [
-                'padding: 1px; color: #FFFFFF; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #3E82E5; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #fdda9c; background: #332a00; border-color: #665500; border-width: 1px 0 1px 0; border-style: solid;'
-            ],
-            'err': [
-                'padding: 1px; color: #FFFFFF; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #3E82E5; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #FFFFFF; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;',
-                'padding: 1px; color: #d56666; background: #280000; border-color: #5b0000; border-width: 1px 0 1px 0; border-style: solid;'
-            ]
-        }[level];
-    }
 }
 
 module.exports = Logger;
